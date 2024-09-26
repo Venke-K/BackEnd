3d25af8fc3c17a326bf1b57d0689c2c2b30023df
venkatjp62@gmail.com <venkatjp62@gmail.com>
2024-09-26 22:01:14 +0530

    second commit

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from './bcryptjs'
 import jwt from "jsonwebtoken";
 import process from "process";
 // import createToken from "../middleware/authMiddleware.js";
